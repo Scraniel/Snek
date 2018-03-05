d25c7786c0f3ef19fbc34275faf73c7c7c73e415
Danny Lewis <nitomatik@gmail.com>
2018-03-05 10:09:35 -0800

    Main game mechanics done

--- Snek-WebApp/src/Snek-WebApp/wwwroot/SnekScripts/Scene/Scene.js
+++ Snek-WebApp/src/Snek-WebApp/wwwroot/SnekScripts/Scene/Scene.js
@@ -8,6 +8,10 @@
     var _centerX = RoundToNearest(_width / 2, SEGMENT_WIDTH);
     var _centerY = RoundToNearest(_height / 2, SEGMENT_WIDTH);
     var _snake = new Snake(_centerX, _centerY);
+
+    // I think this should be a set of points or dictionary from point -> boolean.
+    // That way constant time lookup instead of iteration can be done.
+    //
     var _food = [];
 
     this.GetSceneName = function ()
@@ -24,29 +28,45 @@
             //
             var newX = RoundToNearest(Math.random() * _width, SEGMENT_WIDTH);
             var newY = RoundToNearest(Math.random() * _height, SEGMENT_WIDTH);
-            var newPoint = new Point(newX, newY);
+            var newFood = new Circle(newX, newY, 5);
 
-            if (!Contains(_food, newPoint))
+            if (Contains(_food, newFood) == -1)
             {
-                _food.push(newPoint);
+                _food.push(newFood);
                 amount--;
             }
         }
     };
 
+    this.EndGame = function ()
+    {
+        _snake = new Snake(_centerX, _centerY);
+        _food = [];
+    }
+
     this.Update = function ()
     {
         var x = _snake.GetX();
         var y = _snake.GetY();
+        var foodIndex = ContainsPoint(_food, new Point(x, y));
+
+        if (foodIndex > -1)
+        {
+            // Another reason we should use a set / dictionary
+            //
+            _food.splice(foodIndex, 1);
+            _snake.Eat();
+        }
 
         if (x >= _width || x <= 0 || y >= _height || y <= 0 || _snake.IsDead())
-            _snake = new Snake(_centerX, _centerY);
+            this.EndGame();
         else
             _snake.Move();
 
         var toGenerate = Math.floor((_snake.GetLength() / 5)) + 1 - _food.length;
 
-        if (toGenerate > 0) {
+        if (toGenerate > 0)
+        {
             this.GenerateFood(toGenerate);
         }
     };
@@ -66,14 +86,19 @@
 
         // Draw food
         //
-        for (var i = 0; i < _food.length; i++) {
-            context.arc(_food[i].GetX(), _food[i].GetY(), SEGMENT_WIDTH / 2, 0, Math.PI * 2);
+        for (var i = 0; i < _food.length; i++)
+        {
+            var foodLocation = _food[i].GetLocation();
+
+            context.beginPath();
+            context.arc(foodLocation.GetX(), foodLocation.GetY(), _food[i].GetRadius(), 0, Math.PI * 2);
             context.fill();
         }
 
         // Draw snake
         //
-        for (i = 0; i < toDraw.length; i++) {
+        for (i = 0; i < toDraw.length; i++)
+        {
             context.fillRect(toDraw[i][0], toDraw[i][1], SEGMENT_WIDTH, SEGMENT_WIDTH);
         }
 
--- Snek-WebApp/src/Snek-WebApp/wwwroot/SnekScripts/Utility/Util.js
+++ Snek-WebApp/src/Snek-WebApp/wwwroot/SnekScripts/Utility/Util.js
@@ -24,6 +24,38 @@ function Point(x, y)
     }
 };
 
+function Circle(x, y, radius)
+{
+    var _location = new Point(x, y);
+    var _radius = radius;
+
+    this.ContainsPoint = function (point)
+    {
+        var lowerBoundX = _location.GetX() - _radius;
+        var upperBoundX = _location.GetX() + _radius;
+        var lowerBoundY = _location.GetY() - _radius;
+        var upperBoundY = _location.GetY() + _radius;
+
+        return point.GetX() >= lowerBoundX && point.GetX() <= upperBoundX
+            && point.GetY() >= lowerBoundY && point.GetY() <= upperBoundY;
+    }
+
+    this.Equals = function (otherCircle)
+    {
+        return _location.Equals(otherCircle.GetLocation()) && _radius === otherCircle.GetRadius();
+    }
+
+    this.GetLocation = function ()
+    {
+        return _location;
+    }
+
+    this.GetRadius = function ()
+    {
+        return _radius;
+    }
+};
+
 function Contains(array, value)
 {
     var useEquals = typeof value.Equals === 'function';
@@ -31,8 +63,19 @@ function Contains(array, value)
     for (var i = 0; i < array.length; i++)
     {
         if (useEquals ? array[i].Equals(value) : array[i] === value)
-            return true;
+            return i;
+    }
+    
+    return -1;
+};
+
+function ContainsPoint(array, point)
+{
+    for (var i = 0; i < array.length; i++)
+    {
+        if (array[i].ContainsPoint(point))
+            return i;
     }
 
-    return false;
-};
+    return -1;
+}
